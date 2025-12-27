43149ccb3c450eb44f2648bb6c22bee935930478
Manuel Odendahl <wesen@ruinwesen.com>
2025-12-26 23:01:57 -0500

    TUI: add DefaultDeps (clipboard/time)

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/bmatcuk/doublestar/v4 v4.9.1 // indirect
 	github.com/charmbracelet/bubbles v0.21.0 // indirect
---
+++ internal/tui/app/default_deps.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"time"
+
+	"github.com/atotto/clipboard"
+)
+
+// DefaultDeps is the production dependency set for the TUI.
+//
+// Clipboard behavior depends on the host OS and available clipboard utilities;
+// callers should surface any returned error to the user via a toast.
+type DefaultDeps struct{}
+
+func (DefaultDeps) Now() time.Time { return time.Now() }
+
+func (DefaultDeps) ClipboardWriteAll(text string) error {
+	return clipboard.WriteAll(text)
+}
+
+
